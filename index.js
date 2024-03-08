98bf8d8f475e36af865be190af6752f3ac046c4f
Gordienko0 <2018danil@gmail.com>
2024-03-08 16:20:09 +0300

    test

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, "static")));
+// app.use(express.static(path.resolve(__dirname, "static")));
+app.use(express.static("/var/www/html/static"));
+
 app.use(fileUpload({}));
 app.use("/api", router);
 
